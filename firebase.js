8103bb36891502663643c093aea014572389fb77
ramya <ramya.rolly@gmail.com>
2020-08-04 07:40:41 -0400

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBp5XbPT74fCfZ0rKUKnj-LEYqgSkOXdXE",
+    authDomain: "tiktok-clone-463ec.firebaseapp.com",
+    databaseURL: "https://tiktok-clone-463ec.firebaseio.com",
+    projectId: "tiktok-clone-463ec",
+    storageBucket: "tiktok-clone-463ec.appspot.com",
+    messagingSenderId: "772821562718",
+    appId: "1:772821562718:web:098fe37f77dcadb81c4bcf",
+    measurementId: "G-4CQXQTT4TK"
+  };
